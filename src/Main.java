b4b3d1c7370cc0af3d4be1fbf5a4997f31903507
DariaVydyborets <daria.vydyborets@gmail.com>
2022-09-26 21:22:19 +0300

    My commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
+    //Part1: method for array input by user
+    public static int[] newArray(int lengthOfNewArray) {
+        System.out.println("Please enter " + lengthOfNewArray + " integers");
+        Scanner scanner = new Scanner(System.in);
+        int[] userArray = new int[lengthOfNewArray];
+        for (int i = 0; i < lengthOfNewArray; i++) {
+            userArray[i] = scanner.nextInt();
+        }
+        return userArray;
+    }
+
+    // Part2: array with random values method
+    public static int[] randomArray(int lengthOfRandomArray) {
+        Random random = new Random();
+        int[] ranArray = new int[lengthOfRandomArray];
+        for (int i = 0; i < lengthOfRandomArray; i++) {
+            ranArray[i] = random.nextInt(100);
+        }
+        return ranArray;
+    }
+
+    // Part3: print array method
+    public static void printArray(int[] array) {
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Part4: sum of array elements
+        System.out.println("Please enter the array length for your array");
+        Scanner scanner = new Scanner(System.in);
+        int lengthOfNewArray = scanner.nextInt();
+        int[] userArray = newArray(lengthOfNewArray);
+        int sum = 0;
+        for (int i = 0; i < lengthOfNewArray; i++) {
+            sum += userArray[i];
+        }
+        System.out.println("The sum of your array is " + sum);
+        //Part5: largest value in an array
+        System.out.println("Please enter the array length for random");
+        int lengthOfRandomArray = scanner.nextInt();
+        int[] ranArray = randomArray(lengthOfRandomArray);
+        printArray(ranArray);
+        int largestNumber = 0;
+        for (int i = 0; i < lengthOfRandomArray; i++) {
+            if (largestNumber < ranArray[i]) {
+                largestNumber = ranArray[i];
+            }
+
+        }
+        System.out.println("The largest number of random array is " + largestNumber);
+
+
     }
 }
